a208e970e4aa41616d581142b2aff9aca0b91e10
Hatice Tosun <haticetosuntr@gmail.com>
2020-03-13 22:18:22 -0400

    Initial commit

---
+++ src/main/java/pageObjectModel/AbstractClass.java
@@ -0,0 +1,56 @@
+package pageObjectModel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.Driver;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class AbstractClass {
+
+    //   create a reusable method for click functionality
+    private WebDriver driver = Driver.getDriver();
+
+    WebDriverWait wait = new WebDriverWait( driver , 10 );
+
+    public void clickFunctionality(WebElement clickButton){
+
+        wait.until( ExpectedConditions.elementToBeClickable( clickButton ) );
+        clickButton.click();
+    }
+
+    //   create a reusable method for sendkeys functionality
+    public void sendKeysFunctionality(WebElement sendKeysButton , String value){
+
+        wait.until( ExpectedConditions.visibilityOf( sendKeysButton ) );
+        sendKeysButton.sendKeys( value );
+    }
+    public int randomNum(int max){
+
+        Random rnd = new Random(  );
+
+        int myRandom = rnd.nextInt(max);
+
+        return myRandom;
+
+    }
+
+    public void clickRandomNum(List<WebElement> listOfElements){
+
+        int rnd = randomNum( listOfElements.size() );
+
+        clickFunctionality( listOfElements.get( rnd ) );
+
+    }
+    public void switchWindows(){
+        Set<String>windows= driver.getWindowHandles() ;
+        for (String)
+    }
+
+
+}
---
+++ src/test/java/stepDefinition/addToCart.java
@@ -0,0 +1,57 @@
+package stepDefinition;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.WebDriver;
+import pageObjectModel.homePage;
+import utilities.Driver;
+import utilities.readProperties;
+
+public class addToCart {
+    WebDriver driver = Driver.getDriver();
+    homePage homePage = new homePage();
+
+    @Given("^Navigate to website$")
+    public void navigate_to_website() {
+        driver.get( readProperties.getData( "URL" ) );
+        driver.manage().window().maximize();
+
+    }
+
+    @And("^Search for \"([^\"]*)\"$")
+    public void search_for(String arg1) {
+        homePage.writeInSeachButton( arg1 );
+    }
+
+    @And("^Click on search button$")
+    public void click_on_search_button() {
+        homePage.clickOnButtonSearch();
+    }
+
+    @And("^Click on any item$")
+    public void click_on_any_item() {
+
+    }
+
+    @And("^Fill all the required fields$")
+    public void fill_all_the_required_fields() {
+
+    }
+
+    @And("^Click on Add to cart button$")
+    public void click_on_Add_to_cart_button() {
+
+    }
+
+    @Then("^Verify I am in the cart page$")
+    public void verify_I_am_in_the_cart_page() {
+
+    }
+
+
+    @And("^Verify I'm in the cart$")
+    public void verifyIMInTheCart() {
+    }
+}
